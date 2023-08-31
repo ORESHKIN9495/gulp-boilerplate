60ff317445d16a0045bc81f8ceb6e1521974ad52
ORESHKIN9495 <orehmoskow@gmail.com>
2023-08-31 13:10:05 +0300

    переделал игру

--- src/assets/js/components/app.js
+++ src/assets/js/components/app.js
@@ -1,166 +1,340 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+const enemyHelth = document.querySelector(".score_enemy_helth > figure");
+const degHelth = document.querySelectorAll(".score_deg_helth > svg");
+const score = document.querySelector(".score_counter");
 
-canvas.width = 500;
-canvas.height = 1000;
+const start = document.querySelector(".start");
+
+const over = document.querySelector(".over");
+
+const toggleStart = document.querySelector(".start > button");
+const toggleReStart = document.querySelector(".over > button");
+
+window.addEventListener("DOMContentLoaded", () => {
+  start.style.display = "flex";
+  toggleStart.addEventListener("click", () => {
+    gameOver();
+    start.style.display = "none";
+  });
+
+  toggleReStart.addEventListener("click", () => {
+    gameOver();
+    over.style.display = "none";
+  });
+});
+
+canvas.width = 600;
+canvas.height = 900;
+
+let game;
 
 const gravity = 0.5;
 
-class Deg {
-  constructor() {
-    this.position = {
-      x: 50,
-      y: 600,
-    };
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+  up: {
+    pressed: false,
+  },
+  down: {
+    pressed: false,
+  },
+};
 
-    this.velocity = {
-      x: 0,
-      y: 1,
-    };
+class Degget {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
 
-    this.width = 30;
-    this.height = 30;
+    this.width = 50;
+    this.height = 50;
+
+    this.score = 0;
+
+    this.lives = 5;
+
+    this.marked = false;
   }
 
   draw() {
     ctx.fillStyle = "black";
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    score.textContent = this.score;
+  }
+
+  collision() {
+    console.log("eeee");
   }
 
   update() {
     this.draw();
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
-
-    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
-      this.velocity.y += gravity;
-    }
   }
 }
 
-class Platform {
-  constructor({ x, y }) {
-    this.position = {
-      x,
-      y,
-    };
+const deg = new Degget({
+  position: {
+    x: canvas.width / 2 - 25,
+    y: canvas.height - 100,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
 
-    this.width = 100;
-    this.height = 10;
+class Enemy {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+
+    this.width = 50;
+    this.height = 50;
+
+    this.helth = 100;
   }
 
   draw() {
-    ctx.fillStyle = "red";
+    ctx.fillStyle = "black";
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    enemyHelth.style.width = `${this.helth}%`;
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
   }
 }
 
-const deg = new Deg();
+const enemies = [];
 
-const platform = new Platform({ x: -100, y: 800 });
+class Ammo {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
 
-const arr = [];
+    this.radius = 10;
+  }
 
-const keys = {
-  right: {
-    pressed: false,
-  },
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+  }
 
-  left: {
-    pressed: false,
-  },
-};
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
 
-let scrollOffset = 0;
-
-let platformOffset = 3;
+const ammoItems = [];
 
 const animate = () => {
-  requestAnimationFrame(animate);
+  game = requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   deg.update();
 
-  platform.draw();
-
-  if (platform.position.x < canvas.width + platform.width) {
-    platform.position.x += platformOffset;
-  } else {
-    platform.position.x = -100;
-  }
-
-  if (keys.right.pressed) {
-    deg.velocity.x = 5;
-  } else if (keys.left.pressed) {
-    deg.velocity.x = -5;
-  } else {
-    deg.velocity.x = 0;
-
-    if (keys.right.pressed) {
-      scrollOffset += 5;
-    } else if (keys.left.pressed) {
-      scrollOffset -= 5;
+  ammoItems.forEach((item, idx) => {
+    if (item.position.y + item.radius <= 0) {
+      setTimeout(() => {
+        ammoItems.splice(idx, 1);
+      }, 0);
+    } else {
+      item.update();
     }
+  });
+
+  deg.velocity.x = 0;
+  deg.velocity.y = 0;
+
+  if (enemies.length <= 0) {
+    enemies.push(
+      new Enemy({
+        position: {
+          x: Math.floor((Math.random() * (canvas.width + canvas.height)) / 2),
+          y: 0,
+        },
+        velocity: {
+          x: 2,
+          y: 1,
+        },
+      })
+    );
   }
 
-  if (
-    deg.position.y + deg.height <= platform.position.y &&
-    deg.position.y + deg.height + deg.velocity.y >= platform.position.y &&
-    deg.position.x + deg.width >= platform.position.x &&
-    deg.position.x <= platform.position.x + platform.width
-  ) {
-    deg.velocity.y = -20;
+  enemies.forEach((e, idx) => {
+    e.position.x += e.velocity.x;
+
+    if (e.position.x + e.width + e.velocity.x <= 0 + e.width) {
+      e.velocity.x = 2;
+    }
+
+    if (e.position.x + e.width + e.velocity.x >= canvas.width) {
+      e.velocity.x = -2;
+    }
+
+    if (e.position.y + e.height + e.velocity.y > canvas.height) {
+      cancelAnimationFrame(game);
+      over.style.display = "flex";
+    }
+
+    if (
+      e.position.x < deg.position.x + deg.width &&
+      e.position.x + e.width > deg.position.x &&
+      e.position.y < deg.position.y + deg.height &&
+      e.position.y + e.height > deg.position.y
+    ) {
+      cancelAnimationFrame(game);
+      e.position.y = 0;
+      (deg.position.y = canvas.height - 100),
+        setTimeout(() => {
+          requestAnimationFrame(animate);
+          deg.lives -= 1;
+
+          degHelth[deg.lives].style.fill = "none";
+        }, 0);
+    }
+
+    ammoItems.forEach((ammo, aIdx) => {
+      if (
+        ammo.position.x < e.position.x + e.width &&
+        ammo.position.x + ammo.radius > e.position.x &&
+        ammo.position.y < e.position.y + e.height &&
+        ammo.position.y + ammo.radius > e.position.y
+      ) {
+        setTimeout(() => {
+          e.helth -= 10;
+
+          if (e.helth == 0) {
+            enemies.splice(idx, 1);
+            e.helth = 100;
+            deg.score += 10;
+          }
+
+          ammoItems.splice(aIdx, 1);
+        }, 0);
+      }
+    });
+
+    if (deg.lives <= 0) {
+      cancelAnimationFrame(game);
+
+      over.style.display = "flex";
+    }
+
+    e.update();
+  });
+
+  if (keys.left.pressed) {
+    deg.velocity.x = -5;
+  } else if (keys.right.pressed) {
+    deg.velocity.x = 5;
+  } else if (keys.up.pressed) {
+    deg.velocity.y = -5;
+  } else if (keys.down.pressed) {
+    deg.velocity.y = 5;
   }
 
-  if (scrollOffset > 600) {
-    console.log("YOUR WIN");
+  if (deg.position.x + deg.width + deg.velocity.x < 0 + deg.width) {
+    deg.velocity.x = 0;
   }
 
-  if (deg.position.y > canvas.height) {
-    location.reload();
+  if (deg.position.x + deg.width + deg.velocity.x > canvas.width) {
+    deg.velocity.x = 0;
+  }
+
+  if (deg.position.y + deg.height + deg.velocity.y > canvas.height) {
+    deg.velocity.y = 0;
+  }
+
+  if (deg.position.y + deg.height + deg.velocity.y < 50) {
+    deg.velocity.y = 0;
   }
 };
 
-animate();
+const gameOver = () => {
+  deg.lives = 5;
+
+  deg.score = 0;
+
+  enemies.forEach((e) => {
+    e.helth = 100;
+
+    e.position.y = 0;
+    deg.position.y = canvas.height - 100;
+  });
+
+  degHelth.forEach((e) => {
+    e.style.fill = "#ff004c";
+  });
+
+  animate();
+};
 
 addEventListener("keydown", (e) => {
-  if (e.code === "KeyD") {
-    keys.right.pressed = true;
-  } else if (e.code === "KeyA") {
-    keys.left.pressed = true;
+  switch (e.key) {
+    case "a":
+      keys.left.pressed = true;
+      break;
+
+    case "d":
+      keys.right.pressed = true;
+      break;
+
+    case "w":
+      keys.up.pressed = true;
+      break;
+
+    case "s":
+      keys.down.pressed = true;
+      break;
+
+    case " ":
+      ammoItems.push(
+        new Ammo({
+          position: {
+            x: deg.position.x + deg.width / 2,
+            y: deg.position.y,
+          },
+          velocity: {
+            x: 0,
+            y: -10,
+          },
+        })
+      );
+
+      break;
   }
 });
 
 addEventListener("keyup", (e) => {
-  if (e.code === "KeyD") {
-    keys.right.pressed = false;
-  } else if (e.code === "KeyA") {
-    keys.left.pressed = false;
+  switch (e.key) {
+    case "a":
+      keys.left.pressed = false;
+      break;
+
+    case "d":
+      keys.right.pressed = false;
+      break;
+
+    case "w":
+      keys.up.pressed = false;
+      break;
+
+    case "s":
+      keys.down.pressed = false;
+      break;
   }
 });
-
-// platforms.forEach((platform) => {
-//   platform.draw();
-
-//   if (platform.position.x < canvas.width + platform.width) {
-//     platform.position.x += platformOffset;
-//   } else {
-//     platform.position.x = -100;
-//   }
-// });
-
-// if (platform.position.x < canvas.width - platform.height) {
-//   platform.position.x += platformOffset;
-// } else {
-//   platform.position.x = -100;
-// }
-
-// if (platform.position.x > canvas.width - 200) {
-//   platforms.push(
-//     new Platform({
-//       x: Math.floor(Math.random() + platform.position.x) + 200,
-//       y: platform.position.y - 100,
-//     })
-//   );
-
-//   platforms.slice(platforms.length - 1);
-// }
